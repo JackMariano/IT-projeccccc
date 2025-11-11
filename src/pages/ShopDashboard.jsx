a37ccc084bf9fda596f84c3fe8de08a24df0da82
Jackson Mariano <2192114@slu.edu.ph>
2025-11-12 02:29:41 +0800

    dependencies

---
+++ src/pages/ShopDashboard.jsx
@@ -0,0 +1,8 @@
+// src/pages/ShopDashboard.jsx
+import React from "react";
+
+function ShopDashboard({ user }) {
+  return <div>Welcome, {user.username}!</div>;
+}
+
+export default ShopDashboard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // <-- Make sure your src folder is included
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
